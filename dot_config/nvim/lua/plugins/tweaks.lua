7304f327d0752b7428b527823aaf3af495840927
Zero Code <praveenblogging@gmail.com>
2024-02-12 13:51:01 +0530

    Auto Dots Update Commit 12.02.2024 um 13:51

---
+++ dot_config/nvim/lua/plugins/tweaks.lua
@@ -0,0 +1,32 @@
+return {
+  -- add gruvbox
+  {
+    "ellisonleao/gruvbox.nvim",
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      require("gruvbox").setup({
+        contrast = "hard",
+        transparent_mode = true,
+      })
+    end,
+  },
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "gruvbox",
+    },
+  },
+  -- Turn hidden files on
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      filesystem = {
+        filtered_items = {
+          hide_dotfiles = false,
+        },
+      },
+    },
+  },
+}
