56e81244eeb2dc22de1c0f04923559702907d0c4
Zero Code <pravinblogging@gmail.com>
2024-02-20 12:00:01 +0530

    Auto Dots Update Commit 20.02.2024 um 12:00

--- dot_config/nvim/lua/user/mappings.lua
+++ dot_config/nvim/lua/user/mappings.lua
@@ -17,6 +17,11 @@ return {
     --   function() require("astronvim.utils.buffer").nav(-(vim.v.count > 0 and vim.v.count or 1)) end,
     --   desc = "Previous buffer",
     -- },
+    --
+    ["<A-i>"] = {
+      "<cmd>ToggleTerm<CR>",
+      desc = "Toggle Terminal",
+    },
     ["<A-r>"] = {
       "<cmd>SearchBoxReplace<CR>",
       desc = "Search and Replace",
@@ -24,9 +29,9 @@ return {
     -- mappings seen under group name "Buffer"
     ["<leader>bD"] = {
       function()
-        require("astronvim.utils.status").heirline.buffer_picker(
-          function(bufnr) require("astronvim.utils.buffer").close(bufnr) end
-        )
+        require("astronvim.utils.status").heirline.buffer_picker(function(bufnr)
+          require("astronvim.utils.buffer").close(bufnr)
+        end)
       end,
       desc = "Pick to close",
     },
