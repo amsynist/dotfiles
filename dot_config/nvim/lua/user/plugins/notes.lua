e820ac43e1e0de188d22e43973a850ad6ce7b8f7
Zero Code <pravinblogging@gmail.com>
2024-02-20 13:00:00 +0530

    Auto Dots Update Commit 20.02.2024 um 13:00

--- dot_config/nvim/lua/user/plugins/notes.lua
+++ dot_config/nvim/lua/user/plugins/notes.lua
@@ -1,14 +1,23 @@
 return {
-
-  { import = "astrocommunity.note-taking.neorg" },
-  {
-    "nvim-neorg/neorg",
-    opts = {
-      ["core.dirman"] = {
-
-        load = {
+  "nvim-neorg/neorg",
+  build = ":Neorg sync-parsers",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  event = "VeryLazy",
+  opts = {
+    load = {
+      ["core.defaults"] = {}, -- Loads default behaviour
+      ["core.concealer"] = {}, -- Adds pretty icons to your documents
+      ["core.keybinds"] = {}, -- Adds default keybindings
+      ["core.completion"] = {
+        config = {
+          engine = "nvim-cmp",
+        },
+      },                  -- Enables support for completion plugins
+      ["core.journal"] = {}, -- Enables support for the journal module
+      ["core.dirman"] = { -- Manages Neorg workspaces
+        config = {
           workspaces = {
-            notes = "~/ZERO/NOTES",
+            notes = "~/projects/notes",
           },
         },
       },
