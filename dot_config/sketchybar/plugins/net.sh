bb9077190dfa0e2d0dd575d8a090bcbefc165e5b
= <=>
2024-06-10 17:05:50 +0530

    Remove .config/sketchybar/sketchybarrc Remove .config/sketchybar/plugins/date.sh Remove .config/sketchybar/plugins/net.sh Remove .config/sketchybar/plugins/play_pause.sh Remove .config/sketchybar/plugins/power.sh Remove .config/sketchybar/plugins/spotifyIndicator.sh Remove .config/sketchybar/plugins/time.sh Remove .config/sketchybar/plugins/window_title.sh

--- dot_config/sketchybar/plugins/net.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-
-LABEL=$(networksetup -getairportnetwork en0 | cut -c 24-)
-
-sketchybar --set $NAME label=$LABEL
--- dot_config/sketchybar/plugins/play_pause.sh
+++
@@ -1,7 +0,0 @@
-osascript -e 'tell application "Spotify"
-    if player state is playing then
-        pause
-    else
-        play
-    end if
-end tell'
